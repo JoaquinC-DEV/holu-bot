b4bbc043f95a373396b9a0435446a8380a39fb07
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-04-05 17:52:27 -0400

    comandos y eventos

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,9 @@
 require("dotenv").config();
+
+// Events and Commands registry
+const { registerCommands, registerEvents } = require("./utils/registry.js");
+
+// Discord
 const Discord = require("discord.js-light");
 
 // Bot
@@ -10,5 +15,24 @@ const bot = new Discord.Client({
 bot.botVersion = "0.0.1";
 bot.devs = ["883720498272403516"];
 
-// Discord Login
-bot.login();
+(async () => {
+    bot.commands = new Map();
+    bot.cachedMessageReactions = new Map();
+
+    bot.login();
+    await registerEvents(bot, "../events");
+    await registerCommands(bot, "../commands");
+})();
+
+// Posibles errores
+process.on("unhandledRejection", error => {
+    console.error("Unhandled promise rejection:", error);
+    //This will be useful to finding unknown errors sometimes
+    if (error.requestData?.json) console.error(inspect(error.requestData.json, { depth: 5 }));
+});
+  
+process.on("uncaughtException", err => {
+    bot.destroy();
+    console.error("Uncaught exception:", err);
+    process.exit(1);
+});
---
+++ src/commands/test/test.js
@@ -0,0 +1,7 @@
+module.exports = {
+    run: async (bot, message) => {
+        return message.reply("El comando funciona!");
+    },
+    aliases: ["prueba"],
+    description: "Este es un comando de prueba"
+}
