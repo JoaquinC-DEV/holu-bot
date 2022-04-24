727e91d8c15fb8a00f83fa0237ea1d3878e65c6c
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-04-24 14:42:19 -0400

    eval command :D

---
+++ src/commands/dev/eval.js
@@ -0,0 +1,42 @@
+import Discord from "discord.js-light";
+import util from "util";
+
+export default class extends Command {
+    constructor(options) {
+        super(options);
+        this.aliases = ["e"];
+        this.secret = true;
+        this.description = "Ejecuta scripts rápidamente.";
+        this.dev = true;
+    }
+
+    async run (bot, message, args) {
+        if (!args[1]) return message.channel.send("Te faltó algo...");
+    try {
+        let evaluated = eval(args.slice(1).join(" "));
+        if (evaluated instanceof Promise) {
+            const m = message.channel.send("Evaluando promise...");
+            evaluated.then((async e => {
+                let evaluated = e;
+                if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+                const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950, char: "" });
+                (await m).edit(Discord.Formatters.codeBlock("js", arr[0]));
+            })).catch((async e => {
+                let evaluated = e;
+                if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+                const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950, char: "" });
+                (await m).edit(Discord.Formatters.codeBlock("js", arr[0]));
+            }));
+        } else {
+            if (typeof evaluated !== "string") evaluated = util.inspect(evaluated, { depth: 0 });
+            const arr = Discord.Util.splitMessage(evaluated, { maxLength: 1950, char: "" });
+            await message.channel.send(Discord.Formatters.codeBlock("js", arr[0]));
+        }
+    } catch (err) {
+            let algo = err;
+            if (typeof algo !== "string") algo = util.inspect(algo, { depth: 0 });
+            const arr = Discord.Util.splitMessage(algo, { maxLength: 1950, char: "" });
+            await message.channel.send(Discord.Formatters.codeBlock("js", arr[0]));
+        }
+    }
+}
