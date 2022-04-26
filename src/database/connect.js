ab9d33c61c8bf22369f41bd8f0940e137dc1d48f
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-04-25 20:08:35 -0400

    some changes in the database connection

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,7 +1,13 @@
+import dotenv from "dotenv";
 import mongoose from 'mongoose';
 
-export default async function () {
-    await mongoose.connect(process.env.MDB_PATH);
-    console.log("Connected to the database");
-    return true;
+dotenv.config();
+
+export default function () {
+    mongoose.connect(process.env.MDB_PATH, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Connected to the database"))
+    .catch((err) => console.log("Ocurrió un error al conectarse a la DB: " + err))
 }
--- src/events/misc/ready.js
+++ src/events/misc/ready.js
@@ -2,7 +2,7 @@ import presence from "../../utils/presence.js";
 import connectDB from "../../database/connect.js";
 export default async (bot) => {
     await presence(bot);
-    await connectDB();
+    connectDB();
 
     console.log(`¡${bot.user.tag} está listo! Versión: ${bot.botVersion} (shard ${bot.shard?.ids[0] || 0})`)
 }
