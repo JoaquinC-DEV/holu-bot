a61340d9072449fe7ed32d12c63c5f0bbd1315bb
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-05-11 18:15:28 -0400

    api test

--- src/webserver.js
+++ src/webserver.js
@@ -1,4 +1,5 @@
 import express from "express";
+import profiles from "./database/models/profile.js";
 
 export default function (sharder) {
     const app = express();
@@ -9,6 +10,22 @@ export default function (sharder) {
 
     app.get("/ping", (req, res) => {
         res.send("Hi!");
+    });
+
+    app.get("/profiles/:profileID", (req, res) => {
+        if (isNaN(req.params.profileID) && req.params.profileID.length !== 18) {
+            res.status(400).send("La sintaxis de la ID está mal...");
+        } else {
+            const profile = profiles.findOne({ user_id: req.params.profileID });
+            if (profile) {
+                res.send({
+                    user_id: profile.user_id,
+                    profile_description: profile.description
+                });
+            } else {
+                res.status(404).send("Perfil no encontrado...");
+            }
+        }
     });
 
     const listener = app.listen(process.env.PORT, () => {
