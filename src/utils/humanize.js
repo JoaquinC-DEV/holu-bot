31067c08dfc6155cb0d9e54711ada3dede45716d
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-05-12 13:04:40 -0400

    some fixes

--- src/utils/humanize.js
+++ src/utils/humanize.js
@@ -1,4 +1,13 @@
 import humanize from "humanize-duration";
+
 export default function duration (time) {
-    humanize(time - Date.now(), { units: ['h','m','s'], language: 'es', conjunction: ' y ', serialComma: false, round: true});
+    const timeString = humanize(time - Date.now(), {
+        units: ['h','m','s'],
+        language: 'es',
+        conjunction: ' y ',
+        serialComma: false,
+        round: true
+    });
+
+    return timeString;
 }
