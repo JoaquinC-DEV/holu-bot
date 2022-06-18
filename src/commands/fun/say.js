4ca923983f68c2738f1b9bb5722a2660c7bba62a
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-06-17 22:38:37 -0400

    say command + fun commands category

---
+++ src/commands/fun/say.js
@@ -0,0 +1,19 @@
+export default class extends Command {
+    constructor(options) {
+        super(options);
+        this.aliases = ["decir"];
+        this.secret = false;
+        this.description = "Haz que repita lo que digas.";
+        this.uso = "say [algo]";
+    }
+
+    run (bot, message, args) {
+        if (!args[1]) return message.channel.send("Te faltó poner algo...");
+
+        const mensaje = args.slice(1).join(" ");
+        if (mensaje.includes("discord.gg/")) return message.channel.send("El mensaje al parecer tiene una invitación... ¡No puedo repetir eso!");
+        if (mensaje.length > 2000) return message.channel.send("¡No puedo repetir mensajes tan largos!");
+
+        return message.channel.send(mensaje);
+    }
+}
