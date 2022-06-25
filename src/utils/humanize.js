f3640cb13d99db0102754b155f0dafea6d04441c
JoaquinC-DEV <cancinojoaquin12@gmail.com>
2022-06-25 16:18:53 -0400

    humanize function fix

--- src/utils/humanize.js
+++ src/utils/humanize.js
@@ -2,10 +2,12 @@ import humanize from "humanize-duration";
 
 export default function duration (time) {
     const timeString = humanize(time - Date.now(), {
-        units: ['d', 'h', 'm'],
+        units: ['d', 'h', 'm', 's'],
         language: 'es',
         conjunction: ' y ',
         serialComma: false,
+        round: true,
+        largest: 1
     });
 
     return timeString;
